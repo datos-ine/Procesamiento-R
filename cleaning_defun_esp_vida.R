8883b06e4382c5ad4e784e5ba1569447da0f2468
Tamara Ricardo <161865777+datos-ine@users.noreply.github.com>
2025-05-21 14:58:50 -0300

    Update 21-05-25

--- cleaning_defun_esp_vida.R
+++ cleaning_defun_esp_vida.R
@@ -1,13 +1,15 @@
-### Cálculo de las  defunciones a partir de las bases de datos de mortalidad
-### publicadas por la Dirección de Estadísticas e Información de Salud (DEIS),
-### considerando como causa básica de muerte los códigos E10 a E14 de la Décima
-### Revisión de la Clasificación Estadística Internacional de Enfermedades y
-### Problemas Relacionados con la Salud (CIE-10).
-### Autora: Micaela Gauto 
-### Colaboradora: Tamara Ricardo 
-### Fecha de modificación:
-### Fecha de modificación:
-# Wed May 21 11:18:41 2025 ------------------------------
+### Limpieza y procesamiento de las bases de datos de mortalidad publicadas por
+### la Dirección de Estadísticas e Información de Salud (DEIS), considerando
+### como causa básica de muerte los códigos E10 a E14 de la Décima Revisión de
+### la Clasificación Estadística Internacional de Enfermedades y Problemas
+### Relacionados con la Salud (CIE-10).
+### Limpieza y procesamiento de las tablas de vida publicadas para Argentina en
+### el año 2019 por la GHO-WHO, considerando grupos de edad quinquenales y cada
+### 10 años para población de 20 años y más según sexo.
+### Autoras: Micaela Gauto y Tamara Ricardo
+### Última modificación:
+# Wed May 21 14:53:01 2025 ------------------------------
+
 
 
 # Cargar paquetes ---------------------------------------------------------
@@ -53,6 +55,10 @@ def_raw <- # Listar los csv para cada año de interés
                                 grupo_edad = GRUPEDAD,
                                 total = CUENTA))) |> 
   list_rbind(names_to = "anio")
+
+## Base esperanza de vida GHO-WHO para Argentina
+esp_vida_raw <- read_csv2("Bases de datos/WHO_GHO/argentina_tabla de vida_GHO.csv",
+                          skip = 1)
 
 
 # Limpiar serie defunciones -----------------------------------------------
@@ -139,7 +145,7 @@ def_join <- def04 |>
   select(-causa)
 
 
-# Explorar datos ----------------------------------------------------------
+# Explorar datos defunciones ----------------------------------------------
 tabyl(def_join$prov_nombre)
 
 tabyl(def_join$sexo)
@@ -149,5 +155,47 @@ tabyl(def_join$grupo_edad)
 tabyl(def_join$grupo_edad_10)
 
 
-# Guardar datos limpios ---------------------------------------------------
+
+# Guardar datos defunciones -----------------------------------------------
 write_csv(def_join, "Bases de datos/clean/arg_defun_dm_clean.csv")
+
+
+# Limpiar datos esperanza de vida -----------------------------------------
+esp_vida <- esp_vida_raw |> 
+  # Estandarizar nombres de columna
+  clean_names() |> 
+  
+  # Seleccionar datos para 2019
+  select(indicator,
+         grupo_edad = age_group,
+         "Varón" = male_4,
+         "Mujer" = female_5) |> 
+  
+  # Extraer primeras dos letras del estimador
+  mutate(indicator = str_sub(indicator, start = 1, end = 2)) |>
+  
+  # Crear columna para sexo
+  pivot_longer(cols = c("Varón", "Mujer"), 
+               names_to = "sexo") |>
+  
+  # Crear columnas para cada indicador
+  pivot_wider(names_from = indicator,
+              values_from = value) |> 
+  
+  # Filtrar menores de 20 años y mayores de 85 años
+  filter(!str_detect(grupo_edad, "<1|1-4|5-9|10-14|15-19|85")) |> 
+  
+  # Cambiar las etiquetar grupo edad quinquenal
+  mutate(grupo_edad = fct_relabel(grupo_edad, ~ levels(grupos_edad$grupo_edad))
+  ) |> 
+  
+  # Añadir grupo edad cada 10 años
+  left_join(grupos_edad) |> 
+  select(grupo_edad, grupo_edad_10, everything())
+
+
+# Guardar datos esperanza de vida -----------------------------------------
+write_csv(esp_vida, "Bases de datos/clean/arg_esp_vida_2019_clean.csv")
+
+## Limpiar environment
+rm(list = ls())
